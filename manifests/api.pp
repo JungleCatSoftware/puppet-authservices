d2fb2625c970a6ae09407b4f90c27571eab413c8
Bryan Wyatt <brwyatt@gmail.com>
2016-02-20 14:15:10 -0800

    Add config file to API manifest

--- manifests/api.pp
+++ manifests/api.pp
@@ -1,5 +1,19 @@
 class authservices::api {
 
+  file { '/etc/authservicesapi.conf':
+    ensure  => file,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0444',
+    content => '{
+  "cassandra": {
+    "nodes": [ "10.10.1.25" ]
+  }
+}
+',
+    notify => Service['gunicorn'],
+  }
+
   authservices::pythonwebapp { 'authservicesapi':
     codesource => '/vagrant/src/AuthServices-API',
     bind       => '0.0.0.0:3001',
